3b5134c0a29f1ed963596e000cadf88c2d7f86dd
roxxamay <itsmeadityakumararya@gmail.com>
2024-10-09 16:30:22 +0530

    rofi updated

---
+++ .config/rofi/scripts/launchpower.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the directory containing your scripts
+dir="$HOME/.config/rofi/scripts/powermenu"
+
+# Get a list of all files in the directory and pick one randomly
+random_script=$(find "$dir" -type f | shuf -n 1)
+
+# Execute the randomly chosen script
+bash "$random_script"
